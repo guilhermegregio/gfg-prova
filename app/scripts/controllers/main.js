455e5eface031e19c919eae13887e7167398df80
guilhermegregio <guilherme@3gweb.com.br>
2013-05-17 17:45:31 -0300

    adicionando eventos ao main

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,30 @@
 'use strict';
 
 angular.module('provaClientApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+	.controller('MainCtrl', function ($scope, $http) {
+	$scope.formList = [{
+		id: "xpto",
+		title: "Form bacana",
+		fields: "Nome, Email, Telefone",
+		totalRegistros: "1000"
+	}, {
+		id: "poit",
+		title: "Form angularjs",
+		fields: "Nome, Email, Telefone",
+		totalRegistros: "500000000"
+	}];
+
+	$scope.removeForm = function (formId) {
+		$http({
+			method: 'DELETE',
+			url: '/someUrl'
+		}).
+		success(function () {
+			console.log("SUCCESS");
+		}).
+		error(function () {
+			console.log("ERRO");
+		});
+	}
+
+});
