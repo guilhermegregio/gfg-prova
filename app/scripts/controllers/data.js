9f1f53178ebb803cbb03cdd83cfba0c18e2d0893
guilhermegregio <guilherme@3gweb.com.br>
2013-05-17 18:26:07 -0300

    adicionado forEach nos dados do form dinamicamente

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -2,9 +2,26 @@
 
 angular.module('provaClientApp')
   .controller('DataCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  	$scope.dados = [{
+  		id: "xpto",
+		nome: "Guilherme",
+		telefone: "999999999"
+	},{
+  		id: 'gamgstyle',
+		nome: "Gamg",
+		telefone: "666666666"
+	},{
+  		id: 'editar',
+		nome: "O Formulario",
+		telefone: "666666666",
+		email: "fudeu@gmail.com"
+	},{
+  		id: 'editar',
+		apelido: "fififi",
+		telefone: "666666666",
+	}];
+
+	$scope.nomes = ["id", "nome", "telefone", "email", 'apelido'];
+
   });
