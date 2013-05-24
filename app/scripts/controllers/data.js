e7ada39f632df6ba90e6a26d8c0ee055d8ca45ce
guilhermegregio <guilherme@3gweb.com.br>
2013-05-24 17:17:25 -0300

    data alterado para ler do server

--- app/scripts/controllers/data.js
+++ app/scripts/controllers/data.js
@@ -1,24 +1,10 @@
 'use strict';
 
 angular.module('provaClientApp')
-	.controller('DataCtrl', function ($scope, $routeParams) {
+	.controller('DataCtrl', function ($scope, $routeParams, templateService) {
 
-		console.log($routeParams);
-
-		$scope.dataForm = {
-			fields: [{label: 'id'}, {label: 'nome'}, {label: 'telefone'}, {label: 'email'}, {label: 'apelido'}],
-			data: [{
-				id: '519bd0fde4b0a99234c5c6f2',
-				nome: 'nome1',
-				telefone: '99999-999',
-				email: 'dlbca@gmail.com',
-				apelido: 'apelido1'
-			},
-			{
-				id: '519bd0fde4b0a99234c5c53f',
-				nome: 'nome2',
-				apelido: 'apelido2',
-				idade: 23
-			}]
-		};
+		$scope.dataForm = {};
+		templateService.get({templateId: $routeParams.templateId}, function(templateForm) {
+			$scope.dataForm = templateForm;
+		});
 	});
