828fea69a0c9cf355c9aa865115c897b731b0f76
guilhermegregio <guilherme@3gweb.com.br>
2013-05-29 15:38:28 -0300

    alterando service url

--- app/scripts/services/formTemplateService.js
+++ app/scripts/services/formTemplateService.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('provaClientApp').
 factory('templateService', function($resource) {
-	return $resource('@@host/templates/:templateId/data', {}, {
+	return $resource('@@host/templates/:templateId', {}, {
 		update: {
 			method: 'PUT'
 		}
