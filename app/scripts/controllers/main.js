a5b52b133f8eaca87125911d4c4b1528413bee21
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2013-05-29 16:42:15 -0300

    retirado consoles do main.js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,13 +5,10 @@ angular.module('provaClientApp')
 
 		$scope.formList = [];
 
-		//$scope.$emit('alertEvent', {errors: [{message: 'alguma coisa', category:'teste testes'}]});
-
 		templateService.query(function(templates) {
 			$scope.formList = templates;
 		}, function(data){
 			$scope.$emit('alertEvent', data);
-			$scope.$emit('alertEvent', {errors: [{message: 'alguma coisa', category:'teste testes'},{message: 'alguma coisa', category:'teste testes'},{message: 'alguma coisa', category:'teste testes'}]});
 		});
 
 		$scope.removeForm = function (id) {
