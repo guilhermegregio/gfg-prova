76134b64a9f8572ef5ca75c5404d848e1d2f2ebc
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-05-29 15:20:28 -0300

    fiz url

--- app/scripts/services/formTemplateService.js
+++ app/scripts/services/formTemplateService.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('provaClientApp').
 factory('templateService', function($resource) {
-	return $resource('@@host/templates/:templateId', {}, {
+	return $resource('@@host/templates/:templateId/data', {}, {
 		update: {
 			method: 'PUT'
 		}
