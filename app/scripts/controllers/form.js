50da5144ecb08a5f583ea6bbed60c63fe7ddfdae
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2013-06-03 20:05:33 -0300

    atualizado falerta do form.js

--- app/scripts/controllers/form.js
+++ app/scripts/controllers/form.js
@@ -12,11 +12,51 @@ angular.module('provaClientApp')
 			$scope.$emit('alertEvent', data);
 		});
 
+		var ValidateFields = function (fieldModel) {
+			this.field = fieldModel;
+			this.messageErrors = [];
+
+			this.sendErrors = function () {
+				$scope.$emit('alertEvent', {errors: this.messageErrors});
+			};
+
+			this.addErrors = function () {
+				var field = this.field,
+					messages = [];
+
+				for(var prop in field){
+					if (field[prop] === '' || field[prop] === undefined) {
+						document.querySelector('input').focus();
+						messages.push({message: 'O campo '+prop+' é obrigatório'});
+					}
+				}
+
+				this.messageErrors = messages;
+			};
+
+			this.hasErrors = function () {
+				if (this.messageErrors.length === 0) {
+					return false;
+				}
+
+				return true;
+			};
+
+			this.addErrors();
+		};
+
 		$scope.cadastrar = function () {
 			var postData = {};
 			$scope.viewForm.fields.forEach(function (data) {
 				postData[data.label] = data.value;
 			});
+
+			var validate = new ValidateFields(postData);
+
+			if (validate.hasErrors()) {
+				validate.sendErrors();
+				return false;
+			}
 
 			dataFormService.save({templateId: $routeParams.templateEdit}, postData, function(viewForm) {
 				console.log(viewForm);
